d4d7a33098d4db264ae22c9e47c6ab0a39fc5334
xujiankang6 <1241450485@qq.com>
2019-11-03 16:41:24 +0800

    create my firstItem

---
+++ src/main/java/com/jiankang/testquartz/TestQuartzApplication.java
@@ -0,0 +1,13 @@
+package com.jiankang.testquartz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestQuartzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestQuartzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jiankang/testquartz/TestQuartzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiankang.testquartz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestQuartzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
